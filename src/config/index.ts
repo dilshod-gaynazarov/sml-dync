e20b6a74e5a741761ddfa813af0e661b1cbc6cea
Dilshod Gaynazarov <115635055+dilshod-gaynazarov@users.noreply.github.com>
2024-05-15 14:30:13 +0500

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,11 +3,11 @@ dotenv.config();
 
 export type ConfigType = {
   PORT: number;
-  DB_HOST: string;
-  DB_PORT: number;
-  DB_USER: string;
-  DB_PASS: string;
-  DB_NAME: string;
+  PG_HOST: string;
+  PG_PORT: number;
+  PG_USER: string;
+  PG_PASS: string;
+  PG_DB: string;
   ACCESS_TOKEN_KEY: string;
   ACCESS_TOKEN_TIME: string;
   APP_LOGS_PATH: string;
@@ -18,11 +18,11 @@ export type ConfigType = {
 
 export const config: ConfigType = {
   PORT: Number(process.env.PORT) as unknown as number,
-  DB_HOST: process.env.DB_HOST as unknown as string,
-  DB_PORT: Number(process.env.DB_PORT) as unknown as number,
-  DB_USER: process.env.DB_USER as unknown as string,
-  DB_PASS: process.env.DB_PASS as unknown as string,
-  DB_NAME: process.env.DB_NAME as unknown as string,
+  PG_HOST: process.env.DB_HOST as unknown as string,
+  PG_PORT: Number(process.env.DB_PORT) as unknown as number,
+  PG_USER: process.env.DB_USER as unknown as string,
+  PG_PASS: process.env.DB_PASS as unknown as string,
+  PG_DB: process.env.DB_NAME as unknown as string,
   ACCESS_TOKEN_KEY: process.env.ACCESS_TOKEN_KEY as unknown as string,
   ACCESS_TOKEN_TIME: process.env.ACCESS_TOKEN_TIME as unknown as string,
   APP_LOGS_PATH: process.env.APP_LOGS_PATH as unknown as string,
